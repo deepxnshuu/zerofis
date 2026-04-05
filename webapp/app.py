f3197b37c306bce0dc5ed35aac2b446375024805
Deepanshu <deepanshu.nagle120305@gmail.com>
2026-04-05 18:58:16 +0530

    fix ind error

--- webapp/app.py
+++ webapp/app.py
@@ -195,6 +195,6 @@ def profile():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-    if __name__ == "__main__":
+        
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
