b04dad730e0b6b2f3d749e4dbf2e7f195caf2b85
Deepanshu <deepanshu.nagle120305@gmail.com>
2026-04-05 19:14:40 +0530

    fix path

--- webapp/app.py
+++ webapp/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os
 
-from threat_engine import analyze_url
+from webapp.threat_engine import analyze_url
 
 app = Flask(__name__)
 
